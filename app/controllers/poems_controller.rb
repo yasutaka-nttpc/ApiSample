62fd1ae79fc00d5356718ae4cf8eafe00f6b9eeb
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2017-10-20 00:30:33 +0900

    showを追加

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,16 @@
 class PoemsController < ApplicationController
+  before_action :set_poem, only: [:show]
   def index
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    render json: @poem
+  end
+
+  private
+    def set_poem
+      @poem = Poem.find(params[:id])
+    end
 end
